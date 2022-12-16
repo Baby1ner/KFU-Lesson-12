e0e161ef5b6ac88f82f1c45bd96aacf91719d444
Azatik <azatz739@gmail.com>
2022-12-16 23:53:50 +0300

    Добавьте файлы проекта.

---
+++ KFU-Lesson-12/Bank.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFU_Lesson_12
+{
+    internal class Bank
+    {
+        public enum tip
+        {
+            current,
+            saving
+        }
+        private string Holder;
+        private int Id = 0;
+        private int Balans;
+        private tip Type = tip.current;
+        private static int Idk;
+        private Queue<BankTransaction> queue = new Queue<BankTransaction>();
+
+        public BankTransaction this[int index] => queue.ToArray()[index];
+
+
+        public Bank(tip Type)
+        {
+            Idk++;
+            Id = Idk;
+            this.Type = Type;
+        }
+        public Bank(string Holder, int Balans, tip Type)
+        {
+            Idk++;
+            Id = Idk;
+            this.Balans = Balans;
+            this.Type = Type;
+            this.Holder = Holder;
+        }
+
+        public Bank(int Balans)
+        {
+            Idk++;
+            Id = Idk;
+            this.Balans = Balans;
+        }
+
+
+
+        public string PrintID => $"Создан новый счет в банке, его номер:{Id}";
+        public int GetBalans => Balans;
+        public int GetId => Id;
+        public tip GetTyp => Type;
+        public string holder
+        {
+            get => Holder; 
+            set => Holder = value;
+        }
+
+   
+
+        public void Takeoff()
+        {
+            Console.WriteLine("Введите сумму, которую хотите снять");
+            int off = int.Parse(Console.ReadLine());
+            BankTransaction bankTransaction = new BankTransaction(-off);
+
+            if (Balans > off)
+            {
+                Balans -= off;
+                queue.Enqueue(bankTransaction);
+            }
+            else
+            {
+                Console.WriteLine("Неправильно попробуй еще раз");
+                Takeoff();
+            }
+        }
+        public void Takeon()
+        {
+            Console.WriteLine("Введите сумму, которую хотите внести");
+            int on = int.Parse(Console.ReadLine());
+            BankTransaction bankTransaction = new BankTransaction(on);
+            queue.Enqueue(bankTransaction);
+            Balans += on;
+        }
+        public void transfer(Bank Bank, int sum)
+        {
+            if (Balans > sum)
+            {
+                BankTransaction bankTransaction = new BankTransaction(-sum);
+                BankTransaction bankTransaction2 = new BankTransaction(sum);
+
+                queue.Enqueue(bankTransaction);
+                Balans -= sum;
+                Bank.Balans += sum;
+                Bank.queue.Enqueue(bankTransaction2);
+            }
+            else Console.WriteLine("Не хватает денег для перевода");
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер = {Id}, Тип: {Type}, Баланс: {Balans}");
+        }
+        public void PrintMoneySell()
+        {
+            foreach (BankTransaction item in queue)
+                Console.WriteLine(item.PrintInfo); 
+
+        }
+        
+    }
+}
---
+++ T00Much14(FInally)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T00Much14_FInally_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /
+            Console.WriteLine("Задание 1");
+            Bank bank = new Bank("Azat",10000, Bank.tip.saving);
+            bank.DumpToScreen();
+            
+
+            Console.WriteLine("Задание 2");
+            System.Reflection.MemberInfo inf;
+            inf = typeof(RationalDigits);
+            object[] arr = inf.GetCustomAttributes(false);
+            foreach (object i in arr) 
+            {
+                DeveloperInfoAttribute a = (DeveloperInfoAttribute)i;
+                Console.WriteLine($"Разработчик :{a.Name} \n Время:{DateTime.Parse(a.datereliz)}");
+            }
+            
+            System.Reflection.MemberInfo inf1;
+            inf1 = typeof(Bank);
+            object[] arr1 = inf1.GetCustomAttributes(false);
+            foreach (object i in arr1) 
+            {
+                NameOrgAtribute a = (NameOrgAtribute)i;
+                Console.WriteLine($"Разработчик :{a.Name} \n Организация:{a.Organization}");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
